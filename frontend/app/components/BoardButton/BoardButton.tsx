cb77dd0387542a41967a3eccb5e6fbe6dc20cc56
theWebCookie <jabwojtek@wp.pl>
2024-05-22 12:25:58 +0200

    feat: add BoardButton component

---
+++ frontend/app/components/BoardButton/BoardButton.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import './BoardButton.css';
+
+interface ArrowProps extends React.HTMLProps<HTMLButtonElement> {
+  className: string;
+  path: string;
+  alt: string;
+}
+
+const BoardButton: React.FC<ArrowProps> = ({ className, path, alt }) => {
+  return (
+    <button className={`board-button ${className}`}>
+      <Image src={path} alt={alt} width={20} height={20} />
+    </button>
+  );
+};
+
+export default BoardButton;
