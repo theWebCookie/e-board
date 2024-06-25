e3efa678a93cbab63cedfae3711febb16f5bcc1a
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-06-25 15:27:58 +0200

    feat: login page as home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,15 @@
+import LoginForm from '@/components/LoginForm/LoginForm';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className='flex justify-between max-w-screen-xl max-h-screen'>
+      <section className=''>{/* image section on the left */}</section>
+      <section className='flex align-middle'>
+        <div className='flex flex-col align-middle justify-center'>
+          <h1 className='p-4 pl-0 text-3xl text-center'>Login</h1>
+          <LoginForm />
+        </div>
+      </section>
+    </main>
+  );
 }
