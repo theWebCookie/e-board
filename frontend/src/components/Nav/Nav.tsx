194b9903e4ab46927f494669620b765174288960
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-06-26 10:29:16 +0200

    feat: handle routing

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -5,30 +6,38 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Nav = () => {
   return (
-    <nav>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href='/home' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Tablica</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/history' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Historia</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/profile' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Profil</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <nav className='border-b flex justify-between px-4 items-center h-16'>
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
+              <Link href='/home' legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Tablica</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
+              <Link href='/history' legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Historia</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
+              <Link href='/profile' legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Profil</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div>
+        <span className='flex shrink-0 w-8 h-8 rounded-full overflow-hidden'>
+          <Image src='/avatar.png' alt='avatar' width={32} height={32} className='aspect-squre h-full w-full' />
+        </span>
+      </div>
     </nav>
   );
 };
