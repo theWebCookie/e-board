0e6f2e99e5f7fffb3ce3acb07447278b7ea45324
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-07-15 11:46:38 +0200

    feat: handle chat open

--- frontend/src/components/Arrow/Arrow.tsx
+++ frontend/src/components/Arrow/Arrow.tsx
@@ -3,12 +3,20 @@ import './Arrow.css';
 
 interface ArrowProps extends React.HTMLProps<HTMLButtonElement> {
   className: string;
+  handleChatOpen?: () => void;
 }
 
-const Arrow: React.FC<ArrowProps> = ({ className }) => (
-  <button className={className}>
-    <Image src='/arrow-button.svg' alt='Arrow' width={20} height={20} />
-  </button>
-);
+const Arrow: React.FC<ArrowProps> = ({ className, handleChatOpen }) => {
+  const handleArrowClick = () => {
+    if (handleChatOpen) {
+      handleChatOpen();
+    }
+  };
+  return (
+    <button className={className} onClick={handleArrowClick}>
+      <Image src='/arrow-button.svg' alt='Arrow' width={20} height={20} />
+    </button>
+  );
+};
 
 export default Arrow;
