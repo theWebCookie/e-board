5223086871e6e8576246c1f2ce6407b1f6493756
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-07-15 13:13:49 +0200

    style chat form

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -58,29 +58,29 @@ const Chat: React.FC<IChatProps> = ({ boardName, className }) => {
     <div className={`w-80 p-3 pb-7 bg-slate-100 h-screen ${className}`}>
       <h1 className='mb-1'>{boardName}</h1>
       <Separator />
-      <ScrollArea className='h-full rounded-md p-2'>
-        <div className='my-1'>
-          <Messages messages={messages} currentClientId={clientId} />
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='flex'>
-              <FormField
-                name='message'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Textarea placeholder='Wiadomość..' {...field} className='resize-none min-h-fit h-auto' />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type='submit' disabled={!messageValue.trim()} className='mx-auto'>
-                <Image src='/send.svg' alt='send' width={20} height={20} />
-              </Button>
-            </form>
-          </Form>
-        </div>
+      <ScrollArea className='h-full rounded-md p-1 pb-20'>
+        <Messages messages={messages} currentClientId={clientId} />
       </ScrollArea>
+      <div className='py-3 absolute bottom-0 left-0 bg-slate-200 w-full'>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='flex items-center justify-around w-full'>
+            <FormField
+              name='message'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea placeholder='Wiadomość..' {...field} className='resize-none min-h-fit h-auto' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit' disabled={!messageValue.trim()} className='mr-3'>
+              <Image src='/send.svg' alt='send' width={20} height={20} />
+            </Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 };
