90d20c0ab2626a2daf95c5f9fc9ed471da66707e
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-07-16 10:54:32 +0200

    feat: add auto scroll on recieved message

--- frontend/src/components/Chat/Messages.tsx
+++ frontend/src/components/Chat/Messages.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { IMessage } from './Chat';
 import Message from './Message';
 
@@ -7,11 +8,20 @@ interface IMessagesProps {
 }
 
 const Messages: React.FC<IMessagesProps> = ({ messages, currentClientId }) => {
+  const scrollRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
   return (
     <ul>
       {messages.map((msg, index) => (
         <Message key={index} message={msg.message} isRight={msg.clientId === currentClientId} />
       ))}
+      <li ref={scrollRef}></li>
     </ul>
   );
 };
