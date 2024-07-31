c6ea251cb8d8e8f7dc8d467de6a6bd2c8f7cdeee
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-07-31 12:28:20 +0200

    refactor: improve forms schema

--- frontend/src/components/LoginForm/schema.ts
+++ frontend/src/components/LoginForm/schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 const loginFormSchema = z.object({
-  email: z.string().email('Niepoprawny adres email.'),
-  password: z.string().min(6, 'Hasło musi mieć co najmniej 6 znaków.'),
+  email: z.string().min(1, 'Email jest wymagany.').email('Niepoprawny adres email.'),
+  password: z.string().min(1, 'Hasło jest wymagane.').min(6, 'Hasło musi mieć co najmniej 6 znaków.'),
 });
 
 export default loginFormSchema;
--- frontend/src/components/RegisterForm/schema.ts
+++ frontend/src/components/RegisterForm/schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 const registerFormSchema = z.object({
   name: z.string().min(1, 'Imię jest wymagane.'),
-  email: z.string().email('Niepoprawny adres email.'),
-  password: z.string().min(6, 'Hasło musi mieć co najmniej 6 znaków.'),
+  email: z.string().min(1, 'Email jest wymagany.').email('Niepoprawny adres email.'),
+  password: z.string().min(1, 'Hasło jest wymagane.').min(6, 'Hasło musi mieć co najmniej 6 znaków.'),
 });
 
 export default registerFormSchema;
