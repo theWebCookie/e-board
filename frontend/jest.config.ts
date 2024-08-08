4fd0d5c3e74a0a2616526c8f1c3a4d52c50999b8
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-08-08 09:27:21 +0200

    refactor: use transform instead of babel

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,10 +3,14 @@ import type { Config } from 'jest';
 const config: Config = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
+    '@config': '<rootDir>/../config.js',
   },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
 
 export default config;
