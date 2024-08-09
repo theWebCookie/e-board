58bf1a21bd0ca01b3bee194247de009e2ff003b1
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-08-09 09:36:40 +0200

    chore: install passport

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,66 @@
 import { PrismaClient } from '@prisma/client';
 import express, { Application } from 'express';
+import session from 'express-session';
+import config from 'config';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcrypt';
 import { router } from './routes';
-import { port } from './config';
+
+interface appConfig {
+  port: number;
+  sessionSecret: string;
+}
+
+const appConfig = config.get<appConfig>('app');
 
 const app: Application = express();
+const port = appConfig.port;
 const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: appConfig.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  'local',
+  new LocalStrategy({ usernameField: 'name', passwordField: 'password' }, async (name, password, done) => {
+    const user = await prisma.user.findFirst({ where: { name: name } });
+    console.log('user', user);
+    if (!user) {
+      return done(null, false, { message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.hashed_password);
+    if (!isPasswordValid) {
+      return done(null, false, { message: 'Invalid password' });
+    }
+
+    return done(null, user);
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((id: number, done) => {
+  try {
+    const user = prisma.user.findFirst({ where: { id } });
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
 
 async function main() {
   // const allUsers = await prisma.user.findMany();
