1a3bc469d584f9864a95b01a77e0019e7d054bc5
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-08-09 09:39:13 +0200

    feat: add basic auth

--- backend/src/controllers/account.ts
+++ backend/src/controllers/account.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { saltRounds } from '../config';
+import passport from 'passport';
 
 const prisma = new PrismaClient();
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ error: 'Brak autoryzacji' });
+};
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   const { name, email, password } = req.body;
@@ -27,15 +35,44 @@ export const register = async (req: Request, res: Response): Promise<void> => {
       },
     });
 
-    res.status(201).json({ message: 'Utworzono użytkownika', name: newUser.name });
+    res.status(201).json({ message: 'Utworzono użytkownika', name: newUser.name, sessionId: req.sessionID, id: newUser.id });
   } catch (error) {
     res.status(500).json({ error });
   }
 };
 
-export const login = async (req: Request, res: Response): Promise<void> => {
-  const { email, password } = req.body;
+export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  passport.authenticate('local', async (err: any, user: any, info: any) => {
+    if (err) {
+      res.status(500).json({ error: err });
+      return;
+    }
+    const { email, password } = req.body;
 
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) {
+        res.status(404).json({ error: 'Nie znaleziono użytkownika.' });
+        return;
+      }
+
+      const match = await bcrypt.compare(password, user.hashed_password);
+
+      if (!match) {
+        res.status(401).json({ error: 'Niepoprawne hasło.' });
+        return;
+      }
+
+      res.status(200).json({ message: 'Zalogowano pomyślnie', name: user.name, sessionId: req.sessionID, id: user.id });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  })(req, res, next);
+};
+
+export const userId = async (req: Request, res: Response): Promise<void> => {
+  const { email } = req.body;
   try {
     const user = await prisma.user.findUnique({ where: { email } });
 
@@ -44,14 +81,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
-    const match = await bcrypt.compare(password, user.hashed_password);
-
-    if (!match) {
-      res.status(401).json({ error: 'Niepoprawne hasło.' });
-      return;
-    }
-
-    res.status(200).json({ message: 'Zalogowano pomyślnie', name: user.name });
+    res.status(200).json({ id: user.id });
   } catch (error) {
     res.status(500).json({ error });
   }
